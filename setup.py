46e11110775c62a63758c0731ce5a0f251078566
NoorAmer0 <noorfayyad123@gmail.com>
2024-09-10 10:08:28 +0300

    requirements and setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Arabic-Text-Summarization-AraT5"
+AUTHOR_USER_NAME = "qandos-git"
+SRC_REPO = "TextSummary"
+AUTHOR_EMAIL = "nooramerq0@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for text Summarization",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
